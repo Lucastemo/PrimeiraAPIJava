e17ce978c9173114ffc016b4f11b2169d6085b5d
LucasTM <lucasteixeiramoraes15@gmail.com>
2025-07-24 18:03:59 -0300

    Modificar o retorno dos métodos da API para a resposta ser através de ResponseEntity

--- src/main/java/com/exemplo/primeira_api/controller/TesteController.java
+++ src/main/java/com/exemplo/primeira_api/controller/TesteController.java
@@ -6,6 +6,7 @@ import com.exemplo.primeira_api.service.PessoaService;
 import com.exemplo.primeira_api.dto.MensagemRespostaDto;
 import com.exemplo.primeira_api.dto.PessoaRequestDto;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -21,34 +22,34 @@ public class TesteController {
     PessoaService pessoaService = new PessoaService();
 
     @GetMapping("/teste")
-    public MensagemRespostaDto digaOla(){
-        return new MensagemRespostaDto("Olá, mundo!");
+    public ResponseEntity<?> digaOla(){
+        return ResponseEntity.ok(new MensagemRespostaDto("Olá, mundo!"));
     }
 
     @GetMapping("/pessoas/nome/{id}")
-    public MensagemRespostaDto getNomeOfPessoaById(@PathVariable Integer id){
-        return new MensagemRespostaDto(pessoaService.getNomeById(id));
+    public ResponseEntity<?> getNomeOfPessoaById(@PathVariable Integer id){
+        return ResponseEntity.ok(new MensagemRespostaDto(pessoaService.getNomeById(id)));
     }
 
     @GetMapping("/pessoas/{id}")
-    public PessoaModel GetPessoaById(@PathVariable int id){
-        PessoaModel pessoaEncontrada = pessoaService.getPessoaById(id);
-        return pessoaEncontrada;
+    public ResponseEntity<?> GetPessoaById(@PathVariable int id){
+        return ResponseEntity.ok(pessoaService.getPessoaById(id));
     }
 
     @PostMapping("/pessoas")
-    public MensagemRespostaDto NewPessoa(@RequestBody PessoaRequestDto pessoa){
-        return new MensagemRespostaDto(pessoaService.insertPessoa(pessoa));
+    public ResponseEntity<?> NewPessoa(@RequestBody PessoaRequestDto pessoa){
+        return ResponseEntity.status(201).body(new MensagemRespostaDto(pessoaService.insertPessoa(pessoa)));
     }
 
     @DeleteMapping("/pessoas/{id}")
-    public MensagemRespostaDto DeletePessoa(@PathVariable Integer id){
-        return new MensagemRespostaDto(pessoaService.deletePessoaById(id));
+    public ResponseEntity<Void> DeletePessoa(@PathVariable Integer id){
+        pessoaService.deletePessoaById(id);
+        return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/pessoas/{id}")
-    public MensagemRespostaDto ModifyPessoaById(@PathVariable Integer id, @RequestBody PessoaRequestDto pessoa){
-        return new MensagemRespostaDto(pessoaService.modifyPessoaById(id, pessoa));
+    public ResponseEntity<?> ModifyPessoaById(@PathVariable Integer id, @RequestBody PessoaRequestDto pessoa){
+        return ResponseEntity.ok(new MensagemRespostaDto(pessoaService.modifyPessoaById(id, pessoa)));
     }
 
 }
